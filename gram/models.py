e2cda4ac7ab2f6461796f2636e705d48f32ef526
mwikalikyalo <winifred.kyalo@student.moringaschool.com>
2022-03-05 23:18:52 +0300

    Add the models

--- gram/models.py
+++ gram/models.py
@@ -1,3 +1,19 @@
+from django.utils import timezone
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+  image= models.ImageField(blank=True, null=True)
+  image_names= models.CharField(max_length=100)
+  image_caption= models.CharField(max_length=5000)
+  # likes= 
+  comments= models.CharField(max_length=2000)
+  created_date= models.DateField(default=timezone.now)
+
+  profile= models.ForeignKey('auth.user',on_delete=models.CASCADE)
+  
+  
+class Profile(models.Model):
+  profile_photo= models.ImageField(blank=True, null=True)
+  bio= models.CharField(max_length=1000)
+
