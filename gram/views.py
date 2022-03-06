3f9ea8ee91a7e7386884563569efa68ea1fb1d83
mwikalikyalo <winifred.kyalo@student.moringaschool.com>
2022-03-06 17:49:30 +0300

    Add the page.html template

--- gram/views.py
+++ gram/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Image, Profile
+from django.views.generic import (ListView)
 
 # Create your views here.
+class ImageListView(ListView):
+  template_name= "main/page.html"
+  queryset= Image.objects.all()
+  context_object_name= 'images'
