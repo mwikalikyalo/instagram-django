0612fefb795b16d9ad3eea2d1e2379441081697b
mwikalikyalo <winifred.kyalo@student.moringaschool.com>
2022-03-06 22:28:17 +0300

    Add the image tests

--- gram/tests.py
+++ gram/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from .models import Image, Profile
 
 # Create your tests here.
+class ImagesTestClass(TestCase):
+    def setUp(self):
+        self.post = Profile(name = "_miss.mwikali")
+        self.post.save_profile()
+
+        self.new_image = Image(name = '', image = 'image.png', description = 'This is a random test' , location = self.newYork, category = self.wildlife) 
+
+    def test_instance(self):
+        self.assertTrue(self.new_image, Image)  
+
+    def tearDown(self):
+        Image.objects.all().delete()
+        Profile.objects.all().delete()
+
+    def test_save_image(self):
+        self.new_image.save_image() 
+        images = Image.objects.all()
+        self.assertTrue(len(images) > 0) 
+  
+    def test_delete_method(self):
+        self.post.delete_profile()
+        images = Image.objects.all()
+        self.assertTrue(len(images)==0) 
