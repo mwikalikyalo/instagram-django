7ce74390bb10c0741ecb6c5169337ff5bb341e14
mwikalikyalo <winifred.kyalo@student.moringaschool.com>
2022-03-07 03:36:52 +0300

    ADd navbar structure

--- gram/views.py
+++ gram/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Image, Profile
-from django.views.generic import (ListView)
+from django.views.generic import (
+  ListView
+)
 
 # Create your views here.
 class ImageListView(ListView):
