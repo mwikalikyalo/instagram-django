b963e5eadef901b09d49d51d999517b056f2beb0
mwikalikyalo <winifred.kyalo@student.moringaschool.com>
2022-03-10 03:15:09 +0300

    Add the image form

---
+++ gram/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import Profile, Image, Comments
+
+#create forms
+class ImagesForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        exclude = ['profile', 'created_date']
