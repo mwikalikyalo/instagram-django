a17a191e6549cc99593461a84f7fed1973f2e811
mwikalikyalo <winifred.kyalo@student.moringaschool.com>
2022-03-10 22:28:38 +0300

    Add new migrations

--- gram/models.py
+++ gram/models.py
@@ -7,8 +7,6 @@ class Image(models.Model):
   image= models.ImageField(blank=True, null=True)
   image_names= models.CharField(max_length=100)
   image_caption= models.CharField(max_length=5000)
-  # likes= models.IntegerField()
-  comments= models.CharField(max_length=2000)
   created_date= models.DateField(default=timezone.now)
 
   profile= models.ForeignKey('auth.user',on_delete=models.CASCADE)
@@ -29,11 +27,27 @@ class Profile(models.Model):
 
   def save_profile(self):
       self.save()
-
-
-    
+ 
   def delete_profile(self):
       Profile.objects.filter(id = self.id).delete()
 
   def update_image(self, updates):
       Profile.objects.filter(id = self.id).update(name = updates)
+
+class Comments(models.Model):
+    comment = models.TextField()
+
+    def save_comment(self):
+        self.save()   
+
+    def delete_comment(self):
+        self.save()   
+
+class Likes(models.Model):
+    likes=models.IntegerField()
+
+    def save_likes(self):
+        self.save()
+
+    def delete_likes(self):
+        self.save() 
