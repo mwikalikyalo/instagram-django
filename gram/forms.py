d8331457e637944cccb98100912cd28c923ac2cf
mwikalikyalo <winifred.kyalo@student.moringaschool.com>
2022-03-10 16:14:13 +0300

    Add the signup form

--- gram/forms.py
+++ gram/forms.py
@@ -1,9 +1,15 @@
 from django import forms
-from .models import Profile, Image, Comments
+from .models import Profile, Image
+from django.contrib.auth.models import User
 
 #create forms
 class ImagesForm(forms.ModelForm):
     class Meta:
         model = Image
         exclude = ['profile', 'created_date']
+        fields = ('image_caption', 'image', 'tag_someone',)
 
+class SignUpForm(forms.ModelForm):
+  class Meta:
+    model = Profile
+    exclude = ['bio','profile_pic','profile_avatar','date']
