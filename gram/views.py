062b8e31ac3218c9d220b106ba3174414cf8a002
mwikalikyalo <winifred.kyalo@student.moringaschool.com>
2022-03-11 15:50:50 +0300

    Add the profile views

--- gram/views.py
+++ gram/views.py
@@ -3,12 +3,11 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, authenticate,logout
 from django.contrib import messages
 from django.http  import HttpResponse, Http404
-from .forms import  ImagesForm, CommentsForm
+from .forms import  ImagesForm, CommentsForm, ProfileForm
 from django.contrib.auth.models import User
 from .models import Image, Profile, Comments, Likes
 
 # Create your views here.
-
 @login_required(login_url='/accounts/login/')
 def home(request):
     current_user = request.user
@@ -21,13 +20,46 @@ def home(request):
             image = form.save(commit = False)
             image.profile = Profile.objects.get(user_id = current_user.id)
             image.save()
-
-        return redirect("home") 
-
+        return redirect("page") 
     else:
         form = ImagesForm()    
+    return render(request, 'page.html', {"current_user":current_user, "images": images, "form":form, "liked_images":liked_images})
 
-    return render(request, 'home.html', {"current_user":current_user, "images": images, "form":form, "liked_images":liked_images})
+
+"""New profile page if you are a new user """
+@login_required(login_url='/accounts/login/')
+def create_profile(request):
+    current_user = request.user
+
+    try:
+        Profile.objects.get(user_id = current_user.id)
+        return redirect("page")
+
+    except:
+
+        if request.method == 'POST':
+            form = ProfileForm(request.POST, request.FILES)
+            if form.is_valid():
+                profile = form.save(commit = False)
+                profile.user = current_user
+                profile.save()
+
+            return redirect("page")   
+
+        else:
+            form = ProfileForm()
+
+    return render(request, 'create_profile.html', {"form": form, "current_user":current_user}) 
+
+"""View profile"""
+@login_required(login_url='/accounts/login/')
+def profile(request, username):
+    current_user = request.user
+    profile =  Profile.objects.filter(user = User(id = current_user.id)).first()
+    images = Image.objects.filter(profile = profile.pk ).all() 
+        
+    return render(request, 'profile.html', {"profile": profile, "images": images, "current_user": current_user})    
+
 
 """ Uploading images"""
 @login_required(login_url='/accounts/login/')
@@ -40,33 +72,23 @@ def upload_images(request):
             image = form.save(commit = False)
             image.profile = Profile.objects.get(user_id = current_user.id)
             image.save()
-
-        return redirect("home")   
+        return redirect("page")   
 
     else:
         form = ImagesForm()
-
     return render(request, 'images.html', {"form": form, "current_user":current_user}) 
 
 
+"""Adding a comment"""
+@login_required(login_url='/accounts/login/')
+def comment(request, id):
+     image = Image.objects.get(id = id)
 
+     if request.method == 'POST':
+        form = CommentsForm(request.POST, request.FILES)
+        if form.is_valid():
+            comment = form.save(commit = False)        
+            comment.image = image
+            comment.save()
 
-
-
-
-def user_login(request):
-  if request.method == "POST":
-    username= request.POST['username']
-    password = request.POST['password']
-    user = authenticate(request, username=username, password=password)
-    if user is not None:
-          login(request, user)
-          return redirect('page')
-    else:
-          messages.success(request, 'Cannot login. Try again')
-          return redirect('login')
-
-  else: 
-   return render(request, 'registration/login.html', {})
-
-
+        return redirect("page")   
